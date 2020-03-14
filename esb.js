d63ceddfac32c155aee3fe1ebc57d738e9c888fa
daniiel-morales <daniiel.morales@outlook.com>
2020-03-13 20:49:45 -0600

    add fucntion save msg to queque

--- esb.js
+++ esb.js
@@ -7,32 +7,44 @@
 
 // Modules required
 const http = require('http')
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
-var url = require('url')
+const config = require('./config')
+const Redismq = require('./queque')
+const { Consumer } = require('redis-smq')
 
 // Service parameters
 const host = '127.0.0.1'
 const port = '2013'
 
+var queque = new Redismq.Queque()
+
 var esb = http.createServer(function (req, res) {
-  // parse REST message
-  // msg.speaker -- Who sends me the message?
-  // msg.type -- What do you want me to do?
-  // msg.token -- Need it for access
-  // msg.* -- Parameters need it for operate the request
-  var msg = url.URL(req.url, true).query
-  var xhr
-  switch (msg.speaker) {
-    default:
-      // sample
-      xhr = new XMLHttpRequest()
-      // xhr.open('POST', 'http:// + host + :14810/0', true)
-      xhr.send()
-      break
+  // parses to JSON the url request
+  var msg = JSON.stringify(req.url)
+  // add it to queque
+  if (queque.ADD(msg)) {
+    res.writeHead(200, { 'Content-Type': 'text/json' })
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/json' })
   }
-  // res.writeHead(200, { 'Content-Type': 'text/json' })
   res.end()
 })
 
-// the server order listens on port
+// the esb listens for save requests
 esb.listen(port, host)
+
+console.log('stablished')
+/*
+// the esb operate each request
+class QuequeConsumer extends Consumer {
+  // read the msg request
+  consume (message, cb) {
+    console.log('ESB>> You have work: ', message)
+    cb()
+  }
+}
+
+// set the queue name
+QuequeConsumer.queueName = 'esb-queque'
+
+const consumer = new QuequeConsumer(config, { messageConsumeTimeout: 2000 })
+consumer.run() */
