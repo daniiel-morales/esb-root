8ef7165805e1e0dd67cae33bff60a9b2fcb3b298
daniiel-morales <daniiel.morales@outlook.com>
2020-04-11 15:55:57 -0500

    fix now the esb cosumes and queque only saves it

--- esb.js
+++ esb.js
@@ -6,35 +6,10 @@
 **************************/
 
 // Modules required
-const http = require('http')
 const config = require('./config')
 const Redismq = require('./queque')
 const { Consumer } = require('redis-smq')
 
-// Service parameters
-const host = '127.0.0.1'
-const port = '2013'
-
-var queque = new Redismq.Queque()
-
-var esb = http.createServer(function (req, res) {
-  // parses to JSON the url request
-  var msg = JSON.stringify(req.url)
-  // add it to queque
-  if (queque.ADD(msg)) {
-    res.writeHead(200, { 'Content-Type': 'text/json' })
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/json' })
-  }
-  res.end()
-})
-
-// the esb listens for save requests
-esb.listen(port, host)
-
-console.log('stablished')
-/*
-// the esb operate each request
 class QuequeConsumer extends Consumer {
   // read the msg request
   consume (message, cb) {
@@ -44,7 +19,9 @@ class QuequeConsumer extends Consumer {
 }
 
 // set the queue name
-QuequeConsumer.queueName = 'esb-queque'
+QuequeConsumer.queueName = 'esb_queque'
 
 const consumer = new QuequeConsumer(config, { messageConsumeTimeout: 2000 })
-consumer.run() */
+consumer.run()
+
+console.log('ESB>> started')
