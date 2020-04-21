cf9ddb65f1dec6091835c38937d0ffcdf928a39d
daniiel-morales <daniiel.morales@outlook.com>
2020-04-21 13:52:14 -0500

    add structure for process all standard request

--- esb.js
+++ esb.js
@@ -8,30 +8,66 @@
 // Modules required
 const http = require('http')
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
-var url = require('url')
 
 // Service parameters
-const host = '127.0.0.1'
-const port = '2013'
+const host = process.env.HOST
+const port = process.env.PORT
 
 var esb = http.createServer(function (req, res) {
-  // parse REST message
-  // msg.speaker -- Who sends me the message?
-  // msg.type -- What do you want me to do?
+  // parse REST request
+  // req.url -- For who is the message?
+  // req.method -- What do you want me to do?
   // msg.token -- Need it for access
   // msg.* -- Parameters need it for operate the request
-  var msg = url.URL(req.url, true).query
-  var xhr
-  switch (msg.speaker) {
-    default:
-      // sample
-      xhr = new XMLHttpRequest()
-      // xhr.open('POST', 'http:// + host + :14810/0', true)
-      xhr.send()
-      break
+
+  var msg = ''
+  req.on('data', function(data) {
+    msg = JSON.stringify(JSON.parse(data))
+  })
+
+  if (req.method == 'GET') {
+    switch(req.urltoLowerCase()){
+      case '/vehiculo':
+        break;
+      case '/foto':
+        break;
+      case '/estado':
+        break;
+      case '/afiliado':
+        break;
+      case '/pago':
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+    }
+  }else if (req.method == 'POST') {
+    switch(req.urltoLowerCase()){
+      case '/afiliado':
+        break;
+      case '/pago':
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+    }
+  } else {
+    // PUT
+    switch(req.urltoLowerCase()){
+      case '/vehiculo':
+        break;
+      case '/afiliado':
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+    }
   }
-  // res.writeHead(200, { 'Content-Type': 'text/json' })
-  res.end()
+
+  req.on('end', function() {
+    res.end()
+  })
+
 })
 
 // the server order listens on port
