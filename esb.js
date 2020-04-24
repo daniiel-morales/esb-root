749e6e28625d8fe3d4d6fc161e22ebef8b5e739d
daniiel-morales <daniiel.morales@outlook.com>
2020-04-23 19:40:34 -0600

    add capability for extract parameters fromGET/POST

--- esb.js
+++ esb.js
@@ -7,11 +7,13 @@
 
 // Modules required
 const http = require('http')
+const url = require('url')
+const { parse } = require('querystring')
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 // Service parameters
-const host = process.env.HOST
-const port = process.env.PORT
+const host = 'localhost'//process.env.HOST
+const port = 2013//process.env.PORT
 
 var esb = http.createServer(function (req, res) {
   // parse REST request
@@ -20,65 +22,74 @@ var esb = http.createServer(function (req, res) {
   // msg.jwt -- Need it for access
   // msg.* -- Parameters need it for operate the request
 
-  var msg = ''
-  req.on('data', function(data) {
-    msg = JSON.stringify(JSON.parse(data))
-  })
 
-  var xhttp = new XMLHttpRequest();
+  let msg = ''
+  var starts_parameters = req.url.length
 
-  if (req.method == 'GET') {
-    switch(req.urltoLowerCase()){
-      case '/vehiculo':
-          if(jwt !== undefined){
+  // extracts parameters
+  if(req.method == 'GET'){
+    msg = url.parse(req.url, true).query
+    starts_parameters = req.url.lastIndexOf('?')
+  }else
+    req.on('data', chunk => {
+        msg = parse(chunk.toString())
+    })
 
-          }else{
-            res.writeHead(403, { 'Content-Type': 'application/json' })
-            res.write('{err:\'El JWT no es válido o no contiene el scope de este servicio\'}')
-          }
-        break;
-      case '/foto':
-        break;
-      case '/estado':
-        break;
-      case '/afiliado':
-        break;
-      case '/pago':
-        break;
-      default:
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
-    }
-  }else if (req.method == 'POST') {
-    switch(req.urltoLowerCase()){
-      case '/afiliado':
-        break;
-      case '/pago':
-        break;
-      default:
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
-    }
-  } else {
-    // PUT
-    switch(req.urltoLowerCase()){
-      case '/vehiculo':
-        break;
-      case '/afiliado':
-        break;
-      default:
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
-    }
+  if(jwt !== undefined){
+    // delete parameters of url and the first backslash
+    req.url = req.url.substring(1,starts_parameters).toLowerCase()
+
+    req.on('end', function() {
+
+      var xhr = new XMLHttpRequest();
+
+      if (req.method == 'GET') {
+        switch(req.url){
+          case 'vehiculo':
+            break;
+          case 'foto':
+            break;
+          case 'estado':
+            break;
+          case 'afiliado':
+            break;
+          case 'pago':
+            break;
+          default:
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+        }
+      }else if (req.method == 'POST') {
+        switch(req.url){
+          case 'afiliado':
+            break;
+          case 'pago':
+            break;
+          default:
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+        }
+      } else {
+        // PUT
+        switch(req.url){
+          case 'vehiculo':
+            break;
+          case 'afiliado':
+            break;
+          default:
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.write('{err:\'Resource ' + req.url + ' Not Found 404\'}')
+        }
+      }
+    })
+  }else{
+    res.writeHead(403, { 'Content-Type': 'application/json' })
+    res.write('{err:\'El JWT no es válido o no contiene el scope de este servicio\'}')
   }
-
-  req.on('end', function() {
-    res.end()
-  })
-
+  res.end()
 })
 
 // the esb listens for save requests
 esb.listen(port, host)
 
-console.log('ESB>> started')
+console.log('ESB>> started ' + host + ':' + port )
