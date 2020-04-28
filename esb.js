cb692333631268dce5fe1f0bf31c067c753e3c3f
daniiel-morales <daniiel.morales@outlook.com>
2020-04-28 08:15:58 -0600

    add split for PUBLIC_KEY with '~' separator fix resource not found response and forbbiden response

--- esb.js
+++ esb.js
@@ -41,93 +41,97 @@ var esb = http.createServer(function (req, res) {
         case 'estado':
         case 'afiliado':
         case 'pago':
-          public_key = process.env.PUBLIC_KEY
+          public_key = '-----BEGIN PUBLIC KEY-----\n'
+                      + process.env.PUBLIC_KEY
+                      + '\n-----END PUBLIC KEY-----'
+
+          public_key = public_key.split('~').join('\n')
+
+          let opts = {
+            algorithms: ['RS256']
+          }
+    
+          jwt.verify(msg.jwt, public_key, opts, function(err, decoded) {
+            if (err) {
+              res.writeHead(403, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+              res.write(JSON.stringify({err:'El JWT no es válido o no contiene el scope de este servicio'}))
+              res.end()
+            }else{
+              const scope = JSON.parse(decoded.scope)
+              let access = ''
+              var xhr = new XMLHttpRequest();
+    
+              if (req.method == 'GET') {
+                switch(req_url){
+                  case 'vehiculo':
+                  case 'foto':
+                  case 'estado':
+                    // URL INVENTARIO
+                    API = process.env.URL_INVENTARIO
+                    break;
+                  case 'afiliado':
+                    // URL OFICINA
+                    API = process.env.URL_OFICINA
+                    break;
+                  case 'pago':
+                    // URL SUBASTA
+                    API = process.env.URL_SUBASTA
+                    break;
+                  default:
+                    res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+                    res.write(JSON.stringify({err:'Resource ' + req_url + '.' + req.method.toLowerCase() + ' Not Found 404'}))
+                    res.end()
+                }
+              }else if (req.method == 'POST') {
+                switch(req_url){
+                  case 'afiliado':
+                  case 'pago':
+                    // URL OFICINA
+                    API = process.env.URL_OFICINA
+                    break;
+                  default:
+                    res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+                    res.write(JSON.stringify({err:'Resource ' + req_url + '.' + req.method.toLowerCase() + ' Not Found 404'}))
+                    res.end()
+                }
+              } else {
+                // PUT request
+                switch(req_url){
+                  case 'vehiculo':
+                    // URL INVENTARIO
+                    API = process.env.URL_INVENTARIO
+                    break;
+                  case 'afiliado':
+                    // URL OFICINA 
+                    API = process.env.URL_OFICINA
+                    break;
+                  default:
+                    res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+                    res.write(JSON.stringify({err:'Resource ' + req_url + '.' + req.method.toLowerCase() + ' Not Found 404'}))
+                    res.end()
+                }
+              }
+              access = scope.find(element => element.toLowerCase() == req_url + '.' + req.method.toLowerCase())
+              if(access != undefined){
+                // REQUEST API here
+                callAPI(API, xhr, req.url, msg, req.method, response=>{
+                  res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+                  res.write(response)
+                  res.end()
+                })
+              }else{
+                res.writeHead(403, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+                res.write(JSON.stringify({err:req_url + '.' + req.method.toLowerCase() + ' FORBIDDEN'}))
+                res.end()
+              }
+            }
+          })
           break;
         default:
           res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-          res.write(JSON.stringify({err:'Resource ' + req.url + '.' + req.method + ' Not Found 404'}))
+          res.write(JSON.stringify({err:'Resource ' + req_url + '.' + req.method.toLowerCase() + ' Not Found 404'}))
           res.end()
       }
-
-      let opts = {
-        algorithms: ['RS256']
-      }
-
-      jwt.verify(msg.jwt, public_key, opts, function(err, decoded) {
-        if (err) {
-          res.writeHead(403, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
-          res.write(JSON.stringify({err:'El JWT no es válido o no contiene el scope de este servicio'}))
-          res.end()
-        }else{
-          const scope = JSON.parse(decoded.scope)
-          let access = ''
-          var xhr = new XMLHttpRequest();
-
-          if (req.method == 'GET') {
-            switch(req_url){
-              case 'vehiculo':
-              case 'foto':
-              case 'estado':
-                // URL INVENTARIO
-                API = process.env.URL_INVENTARIO
-                break;
-              case 'afiliado':
-                // URL OFICINA
-                API = process.env.URL_OFICINA
-                break;
-              case 'pago':
-                // URL SUBASTA
-                API = process.env.URL_SUBASTA
-                break;
-              default:
-                res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-                res.write(JSON.stringify({err:'Resource ' + req.url + '.' + req.method + ' Not Found 404'}))
-                res.end()
-            }
-          }else if (req.method == 'POST') {
-            switch(req_url){
-              case 'afiliado':
-              case 'pago':
-                // URL OFICINA
-                API = process.env.URL_OFICINA
-                break;
-              default:
-                res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-                res.write(JSON.stringify({err:'Resource ' + req.url + '.' + req.method + ' Not Found 404'}))
-                res.end()
-            }
-          } else {
-            // PUT request
-            switch(req_url){
-              case 'vehiculo':
-                // URL INVENTARIO
-                API = process.env.URL_INVENTARIO
-                break;
-              case 'afiliado':
-                // URL OFICINA 
-                API = process.env.URL_OFICINA
-                break;
-              default:
-                res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-                res.write(JSON.stringify({err:'Resource ' + req.url + '.' + req.method + ' Not Found 404'}))
-                res.end()
-            }
-          }
-          access = scope.find(element => element.toLowerCase() == req_url + '.' + req.method.toLocaleLowerCase())
-          if(access != undefined){
-            // REQUEST API here
-            callAPI(API, xhr, req.url, msg, req.method, response=>{
-              res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
-              res.write(response)
-              res.end()
-            })
-          }else{
-            res.writeHead(403, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
-            res.write(JSON.stringify({err:'estado.get FORBIDDEN'}))
-            res.end()
-          }
-        }
-      })
     }else{
       res.writeHead(403, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
       res.write(JSON.stringify({err:'El JWT no es válido o no contiene el scope de este servicio'}))
